8983bcf009b35a426dac4c6d8e0bc39ed4e8f35b
MR.Chang <86533019@qq.com>
2017-09-05 15:01:14 +0800

    code review

--- src/main/java/com/example/demo/Demo2.java
+++ src/main/java/com/example/demo/Demo2.java
@@ -1,9 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 /**
  * @author changwenhu
  * @date 2017/8/26
  * @Description ${todo}
  */
+@Controller
+@CrossOrigin//解决跨域问题
 public class Demo2 {
 }
