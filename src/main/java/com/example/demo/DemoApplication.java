676af8a6fa7e12f775f2cf731584154d4f4afaee
MR.Chang <mrchang@jetbrains.org.cn>
2017-10-25 14:42:51 +0800

    The test code is submitted automatically and depends on poll scm

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-        System.out.println(args);
+        System.out.println("The test code is submitted automatically and depends on poll scm");
     }
 }
